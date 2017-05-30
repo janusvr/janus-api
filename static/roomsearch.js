4cc68caee79b54c82d0272e9d7f594f91eb2c2af
Brandon Hinshaw <bkhinshaw@gmail.com>
2017-05-30 13:06:23 +0000

    Get room search working

--- static/roomsearch.js
+++ static/roomsearch.js
@@ -1,24 +1,57 @@
 (function() {
-var xhr = new XMLHttpRequest();
-var keyword = "test";
-var endpoint = "/room/search?keyword="+keyword+"&has_equi=true";
 
-function done(response) {
-    var parsed = JSON.parse(response);
-    console.log(response);
-    console.log(parsed);
-    var div = document.getElementById('images');
-    for (var i = 0; i < parsed.length; i++)
-        div.append(parsed[i].equi)
-}
-
-xhr.onreadystatechange = function() {
-    if (xhr.readystate == 4) {
-            done(xhr.responseText); 
+function updateResults() {
+    var keyword = document.getElementById('keyword').value; 
+    var xhr = new XMLHttpRequest();
+    var endpoint = "/room/search?keyword="+keyword+"&has_equi=true";
+    var div = document.getElementById('rooms');
+ 
+    while (div.firstChild) {
+        div.removeChild(div.firstChild);
     }
+
+    var onData = function(response) {
+        var parsed = JSON.parse(response);
+        for (var i = 0; i < parsed.data.length; i++) {
+            var li = document.createElement("li");
+            li.className = "media";
+            div.append(li);
+
+            var link = document.createElement('a');
+            link.href = "https://web.janusvr.com/sites/"+parsed.data[i].url;
+            link.target = "_blank";
+            li.append(link);
+            
+            var img = document.createElement('img');
+            img.className = "d-flex mr-3 img-fluid";
+            img.src = parsed.data[i].equi;
+            link.append(img);
+
+            var body = document.createElement('div');
+            body.className = "media-body";
+            body.innerHTML += "<h5>"+parsed.data[i].roomtitle+"</h5>";
+            body.innerHTML += parsed.data[i].meta_description;
+            li.append(body);
+        }
+    }
+
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState == XMLHttpRequest.DONE && xhr.status == 200) {
+            onData(xhr.responseText); 
+        }
+    }
+
+    xhr.open('GET', endpoint, true);
+    xhr.send();
 }
 
-xhr.open('GET', endpoint, true);
-xhr.send();
+$(document).ready(function() {
+    var form = document.getElementById('form');
+    form.addEventListener("submit", function(ev) {
+        console.log(ev);
+        ev.preventDefault();
+        updateResults();
+    });
+}); 
 
 })();
